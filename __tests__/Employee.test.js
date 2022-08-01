22cb2d08571c1fbb860b53635b9e33b3057a2b25
samvrny <samvrny@gmail.com>
2022-07-31 20:11:16 -0500

    added the first 'Employee' object and its initial tests, properties, and methods

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,24 @@
+const Employee = require('../lib/Employee');
+
+test('creates an employee object', () => {
+    const employee = new Employee('Rosy', '0', 'frank@gmail.com');
+
+    expect(employee.name).toBe('Rosy');
+    expect(employee.id).toBe('0');
+    expect(employee.email).toBe('frank@gmail.com');
+});
+
+test('gets a name for the employee', () => {
+    const employee = new Employee('Rosy');
+    expect(employee.getName()).toEqual(expect.stringContaining(employee.name));
+});
+
+test('gets an email address for the employee', () => {
+    const employee = new Employee('Rosy');
+    expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email));
+});
+
+test('gets employee id', () => {
+    const employee = new Employee('Rosy');
+    expect(employee.getId()).toEqual(expect.stringContaining(employee.id));
+});
